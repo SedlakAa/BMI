7299118d318f19d66ff74230292c5c6210ff0f8f
Adam Sedlák <Sedlak.Adam@stud.vassboskovice.cz>
2021-10-19 11:16:34 +0200

    Dobrý stav

--- BMI/Program.cs
+++ BMI/Program.cs
@@ -6,7 +6,29 @@ namespace BMI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            Console.WriteLine("Výpočet BMI!");
+
+            Console.ReadLine();
+
+            double hmotnost;
+            double výška;
+            double count;
+            Console.Clear();
+            Console.WriteLine("Výpočet BMI");
+            Console.ReadLine();
+
+            Console.Write("zadej váhu");
+            double.TryParse(Console.ReadLine(), out hmotnost);
+            Console.ReadLine();
+
+            Console.Write("zadej svou výšku");
+            double.TryParse(Console.ReadLine(), out výška);
+            Console.ReadLine();
+
+            count = (hmotnost) / (výška/100*výška/100);
+            Console.WriteLine();
+            Console.WriteLine($"Tvoje BMI je {count} ");
+            Console.ReadLine();
+        }   
     }
 }
